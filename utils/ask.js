fd6f5f0dfaa5d37552c1945e51c81ced8aee88a0
Diego Weinmann <diegoweinmann@gmail.com>
2022-03-06 14:57:53 -0300

    feat: persist user input

--- utils/ask.js
+++ utils/ask.js
@@ -1,37 +1,48 @@
 const fs = require('fs');
+const path = require('path');
+const os = require('os');
 const to = require('await-to-js').default;
 const { Input } = require('enquirer');
 const handleError = require('cli-handle-error');
-const shouldCancel = require('cli-should-cancel')
+const shouldCancel = require('cli-should-cancel');
+const Store = require('data-store');
 
-module.exports = async ({
-	name,
-	message, 
-	hint,
-	initial
-}) => {
+module.exports = async ({ name, message, hint, initial }) => {
+  let history = false;
+  if (!initial && name !== 'name' && name !== 'description') {
+    history = {
+      autosave: true,
+      store: new Store({
+        path: path.join(
+          os.homedir(),
+          `.history/cli-template-creator/${name}.json`
+        ),
+      }),
+    };
+  }
+
   const [err, response] = await to(
     new Input({
-			name,
+      name,
       message,
       hint,
-			initial,
-			validate(value, state) {
-				if (state && state.name === 'name') {
-					if(fs.existsSync(value)) {
-						return `Directory already exists: ./${value}`
-					} else {
-						return true
-					}
-				}
-				
-				return !value ? 'Please add a value' : true
-			}
-    })
-			.on('cancel', () => shouldCancel())
-			.run()
+      initial,
+			history,
+      validate(value, state) {
+        if (state && state.name === 'name') {
+          if (fs.existsSync(value)) {
+            return `Directory already exists: ./${value}`;
+          } else {
+            return true;
+          }
+        }
 
+        return !value ? 'Please add a value' : true;
+      },
+    })
+      .on('cancel', () => shouldCancel())
+      .run()
   );
   handleError('INPUT', err);
-	return response
-}
+  return response;
+};
